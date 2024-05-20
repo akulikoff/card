44bee53884dc39ec01af5a7eef19ee3e8ce0b837
akulikoff <akulikoff86@gmail.com>
2024-05-20 14:06:35 +1000

    vite init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ title: string }>();
+</script>
+
+<template></template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
