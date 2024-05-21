07f9a243949d79003ae61d8aaee9a0242d58f5c1
akulikoff <akulikoff86@gmail.com>
2024-05-21 11:03:50 +1000

    upd

--- src/App.vue
+++ src/App.vue
@@ -20,18 +20,4 @@ const data = {
   />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
-./components/Card.vue
+<style scoped></style>
